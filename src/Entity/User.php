cf6478aeb3b0d812d6e4cd4dd2d14fa296ae9fc1
macineben <macineben15@gmail.com>
2023-03-23 09:26:05 +0100

    Mise a jour table User : user -> i23_users

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+#[ORM\Table(name:'i23_users')]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
