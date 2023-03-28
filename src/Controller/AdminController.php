68f049ecfe68bb2d204f4db2e3b5c32fd37aabda
Léo Hervouet <108589080+leoHerv@users.noreply.github.com>
2023-03-28 17:29:55 +0200

    Add management for EditUsers (not done yet)

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AdminController extends AbstractController
 {
@@ -12,5 +14,51 @@ class AdminController extends AbstractController
     public function addProductAction(): Response
     {
         return $this->render('MainTemplate/Admin/addProduct.html.twig');
+    }
+
+    #[Route('/editUsers', name: 'super_admin_editUsers')]
+    public function editUsersAction(EntityManagerInterface $em): Response
+    {
+        $userRepository = $em->getRepository(User::class);
+        $users = $userRepository->findAll();
+        $args = array(
+            'users' => $users,
+            'currentUser' => $this->getUser()
+        );
+
+        dump($users);
+
+        return $this->render('MainTemplate/Admin/editUsers.html.twig', $args);
+    }
+
+    #[Route(
+        '/suppUser/{idUser}',
+        name: 'admin_suppUser',
+        requirements: ['idUser' => '[1-9]\d*'])]
+    public function suppUserAction(int $idUser, EntityManagerInterface $em): Response
+    {
+        $currentUser = $this->getUser();
+
+        $userRepository = $em->getRepository(User::class);
+        $userToSupp = $userRepository->find($idUser);
+        if(!empty($userToSupp))
+        {
+            $roles = $userToSupp->getRoles();
+
+            if(!(in_array('ROLE_SUPER_ADMIN', $roles) || $currentUser == $userToSupp))
+            {
+                // TODO faire la supression du panier
+
+                $em->remove($userToSupp);
+                $em->flush();
+                $this->addFlash('info', 'The user is remove.');
+            }
+        }
+        else
+        {
+            $this->addFlash('info', 'The user does not exist.');
+        }
+
+        return $this->redirectToRoute('super_admin_editUsers');
     }
 }
--- src/Controller/SuperAdminController.php
+++ src/Controller/SuperAdminController.php
@@ -9,18 +9,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SuperAdminController extends AbstractController
 {
-    #[Route('/editUsers', name: 'super_admin_editUsers')]
-    public function editUsersAction(): Response
-    {
-        return $this->render('MainTemplate/SuperAdmin/editUsers.html.twig');
-    }
-
     #[Route('/addAdmin', name: 'super_admin_addAdmin')]
-    public function addAdminAction(EntityManagerInterface $em , Request $request): Response
+    public function addAdminAction(EntityManagerInterface $em , Request $request, UserPasswordHasherInterface $passwordHasher): Response
     {
         $admin = new User();
         $admin->setRoles(['ROLE_ADMIN']);
@@ -32,6 +27,7 @@ class SuperAdminController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid())
         {
+            $admin->setPassword($passwordHasher->hashPassword($admin, $admin->getPassword()));
             $em->persist($admin);
             $em->flush();
 
