c88a01b46cefec67350c964fc3cba3f7d3d48f9e
macineben <macineben15@gmail.com>
2023-03-28 09:29:22 +0200

    creation formulaire editProfile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
-
+use App\Form\EditProfilFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
     #[Route('/edit', name: 'user_edit')]
-    public function editAction(): Response
+    public function editAction(EntityManagerInterface $em , Request $request , UserPasswordHasherInterface $passwordHasher): Response
     {
-        return $this->render('MainTemplate/User/editProfil.html.twig');
+        $user = $this->getUser();
+        $form = $this->createForm(EditProfilFormType::class , $user);
+        $form->add('send',SubmitType::class, ['label'=> 'Edit Profile']);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $em->persist($user);
+            $em->flush();
+            return $this->render('MainTemplate/Anonyme/accueil.html.twig');
+        }
+        $args = array(
+            'editProfile' => $form->createView(),
+        );
+        return $this->render('MainTemplate/User/editProfil.html.twig',$args);
     }
 
     #[Route('/panier', name: 'user_panier')]
