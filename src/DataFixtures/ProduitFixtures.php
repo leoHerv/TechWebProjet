920b379e7190c45ae843f696abc7ff59817d6345
Léo Hervouet <108589080+leoHerv@users.noreply.github.com>
2023-03-31 15:40:58 +0200

    Add all fixture images

--- src/DataFixtures/ProduitFixtures.php
+++ src/DataFixtures/ProduitFixtures.php
@@ -23,43 +23,43 @@ class ProduitFixtures extends Fixture
             $em->persist($product);
         }
         // Pomme
-        addProduct($em, 'Apple', 1.22, 'other', 100, null);
+        addProduct($em, 'Apple', 1.22, 'other', 100, 'Apple.png');
         // Poire
-        addProduct($em, 'Pear', 3.2, 'other', 120, null);
+        addProduct($em, 'Pear', 3.2, 'other', 120, 'Pear.png');
         // Orange
-        addProduct($em, 'Orange', 0.75, 'citrus', 400, null);
+        addProduct($em, 'Orange', 0.75, 'citrus', 400, 'Orange.png');
         // Pastèque
-        addProduct($em, 'Watermelon', 4.99, 'other', 40, null);
+        addProduct($em, 'Watermelon', 4.99, 'other', 40, 'Watermelon.png');
         // Fraise
-        addProduct($em, 'Strawberry', 0.1472, 'redFruits', 650, null);
+        addProduct($em, 'Strawberry', 0.1472, 'redFruits', 650, 'Strawberry.png');
         // Cerise
-        addProduct($em, 'Cherry', 0.245, 'redFruits', 1200, null);
+        addProduct($em, 'Cherry', 0.245, 'redFruits', 1200, 'Cherry.png');
         // Nectarine
-        addProduct($em, 'Nectarine', 0.45, 'other', 460, null);
+        addProduct($em, 'Nectarine', 0.45, 'other', 460, 'Nectarine.png');
         // Raisin
-        addProduct($em, 'Grape', 0.875, 'other', 85, null);
+        addProduct($em, 'Grape', 0.875, 'other', 85, 'Grape.png');
         // Mangue
-        addProduct($em, 'Mango', 2.872, 'exotic', 70, null);
+        addProduct($em, 'Mango', 2.872, 'exotic', 70, 'Mango.png');
         // Myrtille
-        addProduct($em, 'Blueberry', 0.03374, 'redFruits', 1550, null);
+        addProduct($em, 'Blueberry', 0.03374, 'redFruits', 1550, 'Blueberry.png');
         // Prune
-        addProduct($em, 'Plum', 0.1494, 'other', 630, null);
+        addProduct($em, 'Plum', 0.1494, 'other', 630, 'Plum.png');
         // Banane
-        addProduct($em, 'Banana', 0.12, 'other', 405, null);
+        addProduct($em, 'Banana', 0.12, 'other', 405, 'Banana.png');
         // Framboise
-        addProduct($em, 'Raspberry', 0.0544, 'redFruits', 940, null);
+        addProduct($em, 'Raspberry', 0.0544, 'redFruits', 940, 'Raspberry.png');
         // Kiwi
-        addProduct($em, 'Kiwi', 0.196, 'other', 640, null);
+        addProduct($em, 'Kiwi', 0.196, 'other', 640, 'Kiwi.png');
         // Ananas
-        addProduct($em, 'Pineapple', 3.72, 'exotic', 85, null);
+        addProduct($em, 'Pineapple', 3.72, 'exotic', 85, 'Pineapple.png');
         // Citron
-        addProduct($em, 'Lemon', 0.12, 'citrus', 240, null);
+        addProduct($em, 'Lemon', 0.12, 'citrus', 240, 'Lemon.png');
         // Abricot
-        addProduct($em, 'Apricot', 0.0855, 'other', 585, null);
+        addProduct($em, 'Apricot', 0.0855, 'other', 585, 'Apricot.png');
         // Noix de coco
-        addProduct($em, 'Coconut', 1.19, 'exotic', 130, null);
+        addProduct($em, 'Coconut', 1.19, 'exotic', 130, 'Coconut.png');
         // Peche
-        addProduct($em, 'Peach', 0.5985, 'other', 610, null);
+        addProduct($em, 'Peach', 0.5985, 'other', 610, 'Peach.png');
 
         $em->flush();
     }
--- src/Form/ProductFormType.php
+++ src/Form/ProductFormType.php
@@ -5,11 +5,13 @@ namespace App\Form;
 use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class ProductFormType extends AbstractType
 {
@@ -41,7 +43,30 @@ class ProductFormType extends AbstractType
                                   'Exotic' => 'exotic',
                         ],
                 ])
-            //->add('img')
+            ->add('img',
+                FileType::class, [
+                    'label' => 'Brochure (PDF file)',
+
+                    // unmapped means that this field is not associated to any entity property
+                    'mapped' => false,
+
+                    // make it optional so you don't have to re-upload the PDF file
+                    // every time you edit the Product details
+                    'required' => false,
+
+                    // unmapped fields can't define their validation using annotations
+                    // in the associated entity, so you can use the PHP constraint classes
+                    'constraints' => [
+                        new File([
+                            'maxSize' => '1024k',
+                            'mimeTypes' => [
+                                'image/jpeg',
+                                'image/png',
+                            ],
+                            'mimeTypesMessage' => 'Please upload a valid PDF document',
+                        ])
+                    ],
+                ])
         ;
     }
 
